3e3100015b990b837657eae9ab6592b0cc798e8a
Cole Crown <colecrownwealthadrian@gmail.com>
2024-12-05 14:03:39 +0100

    hero-section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
     return (
-        <div>
-            <h1>Hello</h1>
-        </div>
+        <>
+            <section className="purple_container">
+                <h1 className="heading">
+                    <span className="block">Pitch Your Startup,</span>
+                    <span className="block"> Connect With Entrepreneurs</span>
+                </h1>
+                <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions</p>
+            </section>
+        </>
     );
 }
--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import {auth, signOut, signIn} from "@/public/auth";
+import {auth, signOut, signIn} from "@/auth";
 
 const NavBar = async () => {
     const session = await auth();
